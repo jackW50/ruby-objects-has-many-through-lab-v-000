d3740c2082a435c3b4621c74fceeaf5a565b9771
John Werenski <jtwerenski@gmail.com>
2018-09-29 19:47:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,12 @@
 class Artist
   attr_accessor   
-  attr_reader :name, :songs
+  attr_reader :name
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
-    @songs = []
   end
 
   def self.all
